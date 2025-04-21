35427b87c7f39259b73b761bde2fe7a873833716
DavidTang894 <davidtang894@gmail.com>
2025-04-21 17:17:50 +0800

    init

---
+++ src/App.test.jsx
@@ -0,0 +1,16 @@
+import { expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
+
+test("App form submission", async () => {
+    render(<App />);
+
+    //Get the email input and
+    const emailInput = screen.getByTestId("emailInput");
+    const submitButton = screen.getByTestId("submitButton");
+
+    await userEvent.type(emailInput, 'sigmaschool@gmail.com');
+
+    expect(submitButton.disabled).toBe(false);
+})
